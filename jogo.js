4b10061f33ba0aec95d78d6d65d200d416a180b4
leoakira <leobarros@cjr.org.br>
2018-11-02 19:45:20 -0300

    commit com placar e comentado

--- jogo.js
+++ jogo.js
@@ -35,29 +35,60 @@ var cells = [
         }
     ]
 ];
+var playerone = prompt("Nome do primeiro jogador:");
+var playertwo = prompt("Nome do segundo jogador:");
+
+var playerxElement = document.getElementById("jogadorx"); /** Atribuicao de elementos para o placar do jogo */
+var playeroElement = document.getElementById("jogadoro");
+var winningsxElement = document.getElementById("winningsx");
+var winningsoElement = document.getElementById("winningso");
+var numjogoElement = document.getElementById("numjogo");
+document.getElementById("iconvez").style.backgroundImage = "url('images/x.png')";
 
 var player = 0; /** Variável que determina a qual jogador o turno pertence. Escolheu-se '0' para xis, '1' para bola. */
 var playerImg = ["images/x.png", "images/o.png"];
-var playerWin = ["'X' Wins!", "'O' Wins!"];
+var playerWin = [playerone +" Wins!",playertwo +" Wins!"];
+var winnings = [0,0];
 
 var turn = 0;
+var jogo = 0;
 
-function resetGame ()
+playerxElement.innerHTML = playerone; /** Atribuindo os valores aos elementos do placar */
+playeroElement.innerHTML = playertwo;
+winningsxElement.innerHTML = winnings[0];
+winningsoElement.innerHTML = winnings[1];
+numjogoElement.innerHTML = jogo + 1;
+
+function botaoReset() { /** Funcao do botao de reset que apenas reinicia o turno atual */
+    resetGame();
+    jogo--; /** Compensando o round a mais da funcao resetGame */
+    numjogoElement.innerHTML = jogo + 1;
+    if (jogo % 2 == 0) { /** if e else para nao alterar o jogador e apenas reiniciar o turno atual */
+        player = 0;
+    }
+    else {
+        player = 1;
+    }
+}
+
+
+function resetGame () /** Funcao para resetar o jogo, retirando os x`s e o`s do campo e restaurando os valores da matriz inicial */
 {
     var images = document.getElementsByTagName('img');
     while(images.length > 0) {
-        images[0].parentNode.removeChild(images[0]);
+        images[0].parentNode.removeChild(images[0]); /** Retiracao das imagens */
     }
     for (i = 0; i < 3; ++i)
     {
         for (j = 0; j < 3; ++j)
         {
             document.getElementById(cells[i][j].name).innerHTML = "";
-            cells[i][j].value = 0;
+            cells[i][j].value = 0; /** Resetando os valores da matriz */
         }
     }
-    player = 0;
     turn = 0;
+    jogo++;
+    numjogoElement.innerHTML = jogo + 1; /** Atualizando o round no placar */
 }
 
 /** Quarta parte */
@@ -68,6 +99,8 @@ function checkVictory (i, j) /** Checa se uma linha, coluna ou diagonal foi comp
     var dip = cells[0][0].value + cells[1][1].value + cells[2][2].value;
     var dis = cells[0][2].value + cells[1][1].value + cells[2][0].value;
 
+/** Como utilizamos valores negativos para um jogador e positivo para outro elevamos os valores ao quadrado para saber
+quando qualquer um dos dois completou um trio */
     row *= row;
     col *= col;
     dip *= dip;
@@ -75,7 +108,20 @@ function checkVictory (i, j) /** Checa se uma linha, coluna ou diagonal foi comp
 
     if (row == 9 || col == 9 || dip == 9 || dis == 9)
     {
+
+        winnings[player]++; /** Checando quem ganhou, alertando os jogadores e atualizando o placar */
         alert(playerWin[player]);
+        winningsxElement.innerHTML = winnings[0];
+        winningsoElement.innerHTML = winnings[1];
+
+
+        player = player * (-1) + 1; /** Mudando o jogador */
+        if(player == 0) { /** Atualizando a parte do placar que avisa de quem eh o turno */
+            document.getElementById("iconvez").style.backgroundImage = "url('images/x.png')";
+        }
+        else {
+            document.getElementById("iconvez").style.backgroundImage = "url('images/o.png')";
+        }
         resetGame();
         return 1;
     }
@@ -88,7 +134,7 @@ function updateCell (i, j) /** Atualiza a célula clicada, verifica se houve vit
 {
     cells[i][j].value = (-2) * player + 1;
     var thisCell = document.getElementById(cells[i][j].name);
-    thisCell.innerHTML = "<img src='" + playerImg[player] + "'>";
+    thisCell.innerHTML = "<img src='" + playerImg[player] + "'>"; /** Inserindo as imagens de x ou o nas celulas */
     ++turn;
 
     if (checkVictory(i, j))
@@ -96,11 +142,19 @@ function updateCell (i, j) /** Atualiza a célula clicada, verifica se houve vit
 
     if (turn === 9) {
         alert("Deu Velha!!");
+        player = player * (-1) + 1;
         resetGame();
         return;
     }
 
-    player = player * (-1) + 1;
+    player = player * (-1) + 1; /** Mudando o jogador */
+    if(player == 0) { /** Atualizando aviso de quem possui o turno no placar */
+        document.getElementById("iconvez").style.backgroundImage = "url('images/x.png')";
+    }
+    else {
+        document.getElementById("iconvez").style.backgroundImage = "url('images/o.png')";
+    }
+
 }
 
 /** Segunda parte */
